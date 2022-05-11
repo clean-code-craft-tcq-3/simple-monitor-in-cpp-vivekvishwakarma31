727e7f3013e508f71ffd3762130303a3ed27780b
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-05-11 21:37:57 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,22 +1,12 @@
 #include <assert.h>
 #include <iostream>
+#include "BatteryManagementSystem.h"
+
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
-}
-
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+    BatteryManagementSystem batterymanagementsystem;
+    assert(batterymanagementsystem.BatteryisOK(25, 70, 0.7) == true);
+    assert(batterymanagementsystem.BatteryisOK(50, 85, 0) == false);
+    return 0;
 }
