13a335b46f85eaaece4045c1f457a8b99b3bf95a
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-05-11 21:35:21 +0530

    Create StateOfChargeMonitor.h

---
+++ StateOfChargeMonitor.h
@@ -0,0 +1,30 @@
+#ifndef STATEOFCHARGEMONITOR
+#define STATEOFCHARGEMONITOR
+#include <iostream>
+#include <string>
+#include "BatteryMonitor.h"
+#include "checker.h"
+
+class StateOfChargeMonitor : public BatteryMonitor
+{
+public:
+    bool CheckOptimumLimit(float stateofcharge)
+    {
+        if (stateofcharge < MINIMUMSTATEOFCHARGE || stateofcharge > MAXIMUMSTATEOFCHARGE)
+        {
+            DisplayStatus("State of Charge out of range!");
+            return false;
+
+        }
+        else
+            return true;
+    }
+    void DisplayStatus(std::string content)
+    {
+        std::cout << content << std::endl;
+    }
+
+};
+
+
+#endif
