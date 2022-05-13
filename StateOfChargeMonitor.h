847a82b5fa376d6757aed6aec9ab3ea3e0dbb7bd
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-05-13 10:06:28 +0530

    Update StateOfChargeMonitor.h

--- StateOfChargeMonitor.h
+++ StateOfChargeMonitor.h
@@ -8,7 +8,7 @@
 class StateOfChargeMonitor : public BatteryMonitor
 {
 public:
-    bool CheckOptimumLimit(float stateofcharge)
+    bool IsinOptimumLimit(float stateofcharge)
     {
         if (stateofcharge < MINIMUMSTATEOFCHARGE || stateofcharge > MAXIMUMSTATEOFCHARGE)
         {
