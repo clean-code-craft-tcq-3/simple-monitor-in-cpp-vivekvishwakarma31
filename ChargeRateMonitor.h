4eae07819b005c5de4f5e0691db891e1f8de05af
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-05-18 23:54:20 +0530

    Update ChargeRateMonitor.h

--- ChargeRateMonitor.h
+++ ChargeRateMonitor.h
@@ -4,10 +4,21 @@
 #include <string>
 #include "BatteryMonitor.h"
 #include "checker.h"
-
+#include <iterator>
+#include <map>
+#include <vector>
+using namespace std;
 class ChargeRateMonitor : public BatteryMonitor
 {
+private:
+    vector<string> englishMessages;
+    vector<string> germanMessages;
+    map<LanguageID, vector<string>> warningMessages;
 public:
+    ChargeRateMonitor()
+    {
+        InitializeWarningMessages();
+    }
     bool IsinOptimumLimit(float chargerate)
     {
         if (chargerate > MAXIMUMCHARGERATE)
@@ -23,7 +34,43 @@ public:
     {
         std::cout << content << std::endl;
     }
+        WarningLevel CheckLimitBreached(float chargerate)
+    {
+        float tollerance = getTollerance();
 
+        if ((chargerate >= (MAXIMUMCHARGERATE - tollerance)) && (chargerate <= MAXIMUMCHARGERATE))
+        {
+            return WarningLevel::HigherLimitBreached;
+        }
+
+    }
+
+    float getTollerance()
+    {
+        return ((TOLLERANCEPERCENTAGE*MAXIMUMTEMPERATURE) / 100);
+    }
+
+    void InitializeWarningMessages()
+    {
+        englishMessages.push_back("Charge Rate out of range!");
+        englishMessages.push_back("Warning: Approaching High Charge Rate");
+
+        germanMessages.push_back("Laderate außerhalb des Bereichs!");
+        germanMessages.push_back("Warnung: Annäherung an hohe Laderate");
+        warningMessages.insert(pair<LanguageID, vector<string>>(LanguageID::English, englishMessages));
+        warningMessages.insert(pair<LanguageID, vector<string>>(LanguageID::German, germanMessages));
+    }
+
+    void SendAlert(LanguageID languageID, WarningLevel warningLevel)
+    {
+        // declaring iterators
+        map<LanguageID, vector<string>>::iterator it;
+        it = warningMessages.find(languageID);
+        if (it == warningMessages.end())
+            cout << "Key-value pair not present in map";
+        else
+            cout << it->second.at(warningLevel) << endl;
+    }
 };
 
 
