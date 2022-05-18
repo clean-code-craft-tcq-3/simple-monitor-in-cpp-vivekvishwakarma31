703a6b5353b0dec578cd16546ead12e7fbb3de31
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-05-18 23:50:00 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -1,10 +1,27 @@
 #ifndef CHECKER
 #define CHECKER
 
-#define MINIMUMTEMPERATURE 0
-#define MAXIMUMTEMPERATURE 45
-#define MINIMUMSTATEOFCHARGE 20
-#define MAXIMUMSTATEOFCHARGE 80
+#define MINIMUMTEMPERATURE 0.0f
+#define MAXIMUMTEMPERATURE 45.0f
+#define MINIMUMSTATEOFCHARGE 20.0f
+#define MAXIMUMSTATEOFCHARGE 80.0f
 #define MAXIMUMCHARGERATE 0.8f
+#define LOWERLIMITBREACHED 0
+#define HIGHERLIMITBREACHED 1
+#define TOLLERANCEPERCENTAGE 5.0f
+
+enum WarningLevel
+{
+    OutOfRange = 0,
+    LowerLimitBreached,
+    HigherLimitBreached
+};
+
+enum LanguageID
+{
+  English = 0,
+  German
+};
+
 
 #endif
