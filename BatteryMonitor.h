203f33a218536e3fc9c288e57d906ad4c6525906
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-08-04 21:58:55 +0530

    Update BatteryMonitor.h

--- BatteryMonitor.h
+++ BatteryMonitor.h
@@ -2,11 +2,12 @@
 #define BATTERYMONITOR
 
 #include <iostream>
+#include "checker.h"
 class BatteryMonitor
 {
 public:
     virtual bool IsinOptimumLimit(float) = 0;
-    virtual void DisplayStatus(std::string) = 0;
+    virtual void DisplayStatus(WarningLevel warnLevel) = 0;
 };
 
 
