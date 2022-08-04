2a9474e4aad48fbb73260626db4f9a78a40abb39
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-08-04 22:27:49 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -33,12 +33,28 @@ using namespace std;
    assert(temperatureMonitor.CheckLimitBreached(0.0) == WarningLevel::LowerLimitBreached);
    assert(temperatureMonitor.CheckLimitBreached(45.0) == WarningLevel::HigherLimitBreached);
    assert(temperatureMonitor.IsinOptimumLimit(45.0) == false);
-
-
+ }
+ void testStateOfChagre()
+ {
+   StateOfChargeMonitor stateOfChargeMonitor;
+   assert(stateOfChargeMonitor.checkLowerLimitBreached(20.0) == true);   
+   assert(stateOfChargeMonitor.checkHigherLimitBreached(80.0) == true);
+   assert(stateOfChargeMonitor.CheckLimitBreached(20.0) == WarningLevel::LowerLimitBreached);
+   assert(stateOfChargeMonitor.CheckLimitBreached(80.0) == WarningLevel::HigherLimitBreached);
+   assert(stateOfChargeMonitor.IsinOptimumLimit(80.0) == false);
+ }
+ void testChargeRate()
+ {
+   ChargeRateMonitor chargeRateMonitor;
+   assert(chargeRateMonitor.checkHigherLimitBreached(0.8) == true);
+   assert(chargeRateMonitor.CheckLimitBreached(0.8) == WarningLevel::HigherLimitBreached);
+   assert(chargeRateMonitor.IsinOptimumLimit(0.8) == false);
  }
 
 int main() {
     assert(isBatteryOK(25, 70, 0.7) == true);
     testTemperature();
+    testStateOfChagre();
+    testChargeRate();
     return 0;
 }
